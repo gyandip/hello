40000a986a5beb43bb4274298ca37ef457fcdffa
gyan08dip <gyan08dip@gmail.com>
2016-09-14 18:52:11 +0530

    html link

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
+def index(request):
+
+    return render(request, 'blog/index.html', {})
